12d63894bd6950a0e51c9bb4141015c285107337
gcdavis26 <geoffreycdavis@proton.me>
2025-11-05 14:05:59 -0500

    cmakelists set up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rsky
+
 Team RaspberrySky's drone code for the 2025-2026 PSU autonomous vehicle capstone
+
+
+
+A "libs" folder will need to be created for this code to work. Eigen must then be downloaded from https://eigen.tuxfamily.org/index.php?title=Main\_Page, and unzipped into libs. After this is done, run CMake to build solution. Set "rsky" as startup project, and then run. 
+
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+int main() {
+    // Create a 2x2 matrix
+    Eigen::Matrix2d A;
+    A << 1, 3,
+        3, 4;
+
+    // Print matrix
+    std::cout << "Matrix A:\n" << A << "\n\n";
+
+    // Compute inverse
+    Eigen::Matrix2d A_inv = A.inverse();
+    std::cout << "Inverse of A:\n" << A_inv << "\n\n";
+
+    // Check multiplication
+    std::cout << "A * A_inv:\n" << A * A_inv << std::endl;
+
+    return 0;
+}
