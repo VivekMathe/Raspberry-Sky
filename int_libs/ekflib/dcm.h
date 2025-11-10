d4b7634350ca4e2bfe84f7d6dc9a69948c63c6d5
gcdavis26 <geoffreycdavis@proton.me>
2025-11-10 15:33:08 -0500

    began ekf library

---
+++ int_libs/ekflib/dcm.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <Eigen/Dense>
+Eigen::Matrix<double, 3, 3> dcmI_B(double phi, double theta, double psi);
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
 #include <Eigen/Dense>
 #include <iostream>
-
+#include <ekflib/dcm.h>
 int main() {
-    // Create a 2x2 matrix
-    Eigen::Matrix2d A;
-    A << 1, 3,
-        3, 4;
-
-    // Print matrix
-    std::cout << "Matrix A:\n" << A << "\n\n";
-
-    // Compute inverse
-    Eigen::Matrix2d A_inv = A.inverse();
-    std::cout << "Inverse of A:\n" << A_inv << "\n\n";
-
-    // Check multiplication
-    std::cout << "A * A_inv:\n" << A * A_inv << std::endl;    
-    std::cout << Navio2::addOne(3) << std::endl;
-    return 0;
+	Eigen::Matrix<double,3,3> x = dcmI_B(5, 5, 5);
+	std::cout << x << std::endl;
 }
